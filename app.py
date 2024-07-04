e1d35d446d190b70df3849952623b1a27678ee76
pranathiproddhaturi19 <pranathi6304@gmail.com>
2024-07-04 09:36:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email/SMS Spam Classifier")
+st.title("SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
